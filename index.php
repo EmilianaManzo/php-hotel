338a9d7fe03da6f7371e4fb3a33be08199094688
EmilianaManzo <emiliana.ma96@gmail.com>
2024-04-15 16:42:15 +0200

    stampo card hotel

--- index.php
+++ index.php
@@ -40,6 +40,14 @@
 
     ];
 
+    // foreach($hotels as $hotel){
+    //   $name = $hotel['name'];
+    //   $description = $hotel['description'];
+    //   $parking = $hotel['parking'];
+    //   $vote =$hotel['vote'];
+    //   $distance_to_center =$hotel['distance_to_center'];
+    // }
+  
 ?>
 
 <!DOCTYPE html>
@@ -47,13 +55,23 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css' integrity='sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==' crossorigin='anonymous'/>
+
   <title>PHP Hotel</title>
 </head>
 <body>
   <div class="container">
     <div class="row">
-      <div class="col">
-        
+      <div class="col d-flex ">
+        <?php foreach($hotels as  $hotel): ?>
+        <div class="card">
+          <h1><?php echo $hotel['name'] ?></h1>
+          <p><?php echo $hotel['description'] ?></p>
+          <p>Parcheggio: <?php echo $hotel['parking'] ? 'Si' : 'No' ?></p>
+          <p>Voto: <?php echo $hotel['vote'] ?></p>
+          <p>Distanza dal centro: <?php echo $hotel['distance_to_center'] ?></p>
+        </div>
+        <?php endforeach ?>
       </div>
     </div>
   </div>
